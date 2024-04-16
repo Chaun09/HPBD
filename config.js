3b5519aec4aa050366a59b5f9bd46cf167abc7d2
chaubanban <bangcac24@gmail.com>
2024-04-16 22:20:58 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,35 @@
+
+var config = {
+   
+    texts: [
+        "Bạn Thảo À ",    
+        "Chúc mừng sinh nhật bạn", 
+        "Tuổi mới hay ăn chóng lớn",
+        "Sức khoẻ dồi dào",
+        "Phát lộc phát tài",
+        "Vạn sự như ý",
+        "Sớm Đồng Ý Làm Người Yêu Tôi =))))))))))))))))))))))))))))))))))",
+        "Thật nhiều hạnh phúc",
+        "Công việc suôn sẻ",
+        "Thành Công Trên Con Đường Mình Đã Chọn",
+        "Sống Vui Sống Khỏe Nhé"
+        
+        
+    ],
+
+    
+
+    desc: {
+        turn_on: "Bật đèn",
+        play: "Bật nhạc",
+        bannar_coming: "Hãy trang trí với tôi nào!!!",
+        bannar_coming1:"Ảnh người đẹp nhất hôm nay xuất hiện",
+        balloons_flying: "Thêm bóng bay",
+        cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
+        light_candle: "Thêm nến nào",
+        wish_message: "Happy Birthday",
+        story: "Tôi có lời chúc đến bạn...",
+    },
+
+   
+};
