79ef904e05c24044282bd83d87740f7eb21602cf
Chaun09 <bangcac24@gmail.com>
2025-04-01 08:52:54 +0700

    oanh2

--- config.js
+++ config.js
@@ -1,35 +1,25 @@
-
 var config = {
-   
-    texts: [
-        "Bạn Linh À ",   
-        "Chúc mừng sinh nhật bạn", 
-        "Tuổi mới hay ăn chóng lớn",
-        "Sức khoẻ dồi dào",
-        "Phát lộc phát tài",
-        "Vạn sự như ý",
-        "Đồng ý đi xem phim với toi nhớ :)))))))))"
-        "Thật nhiều hạnh phúc",
-        "Công việc suôn sẻ",
-        "Thành Công Trên Con Đường Mình Đã Chọn nhé",
-        
-        
-        
-    ],
+  texts: [
+    "Bạn Oanh À ",
+    "Chúc mừng sinh nhật bạn nho",
+    "Tuổi mới hay ăn chóng lớn",
+    "Mãi xinh mãi đỉnh nè",
+    "Sức khoẻ dồi dào",
+    "Phát lộc phát tài",
+    "Vạn sự như ý",
+    "Công việc suôn sẻ",
+    "Thành Công Trên Con Đường Mình Đã Chọn nhoa",
+  ],
 
-    
-
-    desc: {
-        turn_on: "Bật đèn",
-        play: "Bật nhạc",
-        bannar_coming: "Hãy trang trí với tôi nào!!!",
-        bannar_coming1:"Ảnh người đẹp nhất hôm nay xuất hiện",
-        balloons_flying: "Thêm bóng bay",
-        cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
-        light_candle: "Thêm nến nào",
-        wish_message: "Happy Birthday",
-        story: "Tôi có lời chúc đến bạn...",
-    },
-
-   
+  desc: {
+    turn_on: "Bật đèn",
+    play: "Bật nhạc",
+    bannar_coming: "Hãy trang trí với tôi nào!!!",
+    bannar_coming1: "Ảnh người đẹp nhất hôm nay xuất hiện",
+    balloons_flying: "Thêm bóng bay",
+    cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
+    light_candle: "Thêm nến nào",
+    wish_message: "Happy Birthday",
+    story: "Tôi có lời chúc đến bạn...",
+  },
 };
